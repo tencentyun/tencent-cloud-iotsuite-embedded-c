db9b874404531c9159bfe05fd65edae595814133
manfredhu <manfredhu@tencent.com>
2018-01-25 22:12:20 +0800

    change include path.

--- include/tc_iot_inc.h
+++ include/tc_iot_inc.h
@@ -4,26 +4,32 @@
 
 #include "MQTTPacket.h"
 #include "MQTTConnect.h"
+
+#ifndef JSMN_PARENT_LINKS
+#define JSMN_PARENT_LINKS 1
+#endif
+
 #include "jsmn.h"
 
 #include "tc_iot_platform.h"
-#include "tc_iot_log.h"
-#include "tc_iot_const.h"
-#include "tc_iot_safety_check.h"
-#include "tc_iot_hmac.h"
-#include "tc_iot_hal_os.h"
-#include "tc_iot_hal_timer.h"
-#include "tc_iot_hal_network.h"
 
-#include "tc_iot_ya_buffer.h"
-#include "tc_iot_http_utils.h" 
-#include "tc_iot_base64.h"
-#include "tc_iot_json.h"
-#include "tc_iot_url.h"
+#include "common/tc_iot_log.h"
+#include "common/tc_iot_const.h"
+#include "common/tc_iot_safety_check.h"
+#include "common/tc_iot_hmac.h"
+#include "platform/tc_iot_hal_os.h"
+#include "platform/tc_iot_hal_timer.h"
+#include "platform/tc_iot_hal_network.h"
+
+#include "common/tc_iot_ya_buffer.h"
+#include "common/tc_iot_http_utils.h" 
+#include "common/tc_iot_base64.h"
+#include "common/tc_iot_json.h"
+#include "common/tc_iot_url.h"
 #include "tc_iot_config.h"
-#include "tc_iot_mqtt.h"
-#include "tc_iot_client.h"
-#include "tc_iot_shadow.h"
-#include "tc_iot_token.h"
+#include "common/tc_iot_mqtt.h"
+#include "common/tc_iot_client.h"
+#include "common/tc_iot_shadow.h"
+#include "common/tc_iot_token.h"
 
 #endif /* end of include guard */
