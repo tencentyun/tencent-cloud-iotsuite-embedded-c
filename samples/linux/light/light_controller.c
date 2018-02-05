19b087762abc8b8fa2845c1c8ad289c0d72e6455
huxinbang <huxinbang@gmail.com>
2018-02-05 15:27:47 +0800

    add light/init

---
+++ samples/linux/light/light_controller.c
@@ -0,0 +1,166 @@
+#include "tc_iot_device_config.h"
+#include "tc_iot_export.h"
+
+#define TC_IOT_TROUBLE_SHOOTING_URL "https://git.io/vN9le"
+
+#define TC_IOT_LIGHT_NAME_LEN  25
+typedef _tc_iot_demo_light {
+    bool          light_switch;
+    char          name[TC_IOT_LIGHT_NAME_LEN];
+    unsigned int  color;
+    float         brightness; // percentage 0.00 ~ 100.00
+}tc_iot_demo_light;
+
+extern void parse_command(tc_iot_mqtt_client_config * config, int argc, char ** argv);
+int run_shadow(tc_iot_shadow_config * p_client_config);
+
+static volatile int stop = 0;
+
+static tc_iot_demo_light light_status = {
+    false, "colorful light", 0xFFFFFF, 100.00,
+};
+
+void sig_handler(int sig) {
+    if (sig == SIGINT) {
+        tc_iot_hal_printf("SIGINT received, going down.\n");
+        stop ++;
+    } else if (sig == SIGTERM) {
+        tc_iot_hal_printf("SIGTERM received, going down.\n");
+        stop ++;
+    } else {
+        tc_iot_hal_printf("signal received:%d\n", sig);
+    }
+    if (stop >= 3) {
+        tc_iot_hal_printf("SIGINT/SIGTERM received over %d times, force shutdown now.\n", stop);
+        exit(0);
+    }
+}
+
+void _on_message_received(tc_iot_message_data* md) {
+    tc_iot_mqtt_message* message = md->message;
+    tc_iot_hal_printf("[s->c] %.*s\n", (int)message->payloadlen, (char*)message->payload);
+}
+
+tc_iot_shadow_config g_client_config = {
+    {
+        {
+            // device info
+            TC_IOT_CONFIG_DEVICE_SECRET, TC_IOT_CONFIG_DEVICE_PRODUCT_ID,
+            TC_IOT_CONFIG_DEVICE_NAME, TC_IOT_CONFIG_DEVICE_CLIENT_ID,
+            TC_IOT_CONFIG_DEVICE_USER_NAME, TC_IOT_CONFIG_DEVICE_PASSWORD, 0,
+        },
+        TC_IOT_CONFIG_SERVER_HOST,
+        TC_IOT_CONFIG_SERVER_PORT,
+        TC_IOT_CONFIG_COMMAND_TIMEOUT_MS,
+        TC_IOT_CONFIG_KEEP_ALIVE_INTERVAL_SEC,
+        TC_IOT_CONFIG_CLEAN_SESSION,
+        TC_IOT_CONFIG_USE_TLS,
+        TC_IOT_CONFIG_AUTO_RECONNECT,
+        TC_IOT_CONFIG_ROOT_CA,
+        TC_IOT_CONFIG_CLIENT_CRT,
+        TC_IOT_CONFIG_CLIENT_KEY,
+        NULL,
+        NULL,
+    },
+    TC_IOT_SUB_TOPIC_DEF,
+    TC_IOT_PUB_TOPIC_DEF,
+    _on_message_received,
+};
+
+int main(int argc, char** argv) {
+    tc_iot_mqtt_client_config * p_client_config;
+    bool token_defined;
+    int ret;
+
+    signal(SIGINT, sig_handler);
+    signal(SIGTERM, sig_handler);
+
+    p_client_config = &(g_client_config.mqtt_client_config);
+    parse_command(p_client_config, argc, argv);
+    snprintf(g_client_config.sub_topic,TC_IOT_MAX_MQTT_TOPIC_LEN, TC_IOT_SUB_TOPIC_FMT, 
+            p_client_config->device_info.product_id,p_client_config->device_info.device_name);
+    snprintf(g_client_config.pub_topic,TC_IOT_MAX_MQTT_TOPIC_LEN, TC_IOT_PUB_TOPIC_FMT, 
+            p_client_config->device_info.product_id,p_client_config->device_info.device_name);
+    token_defined = strlen(p_client_config->device_info.username) && strlen(p_client_config->device_info.password);
+
+    if (!token_defined) {
+        tc_iot_hal_printf("requesting username and password for mqtt.\n");
+        ret = http_refresh_auth_token(
+                TC_IOT_CONFIG_AUTH_API_URL, TC_IOT_CONFIG_ROOT_CA,
+                &p_client_config->device_info);
+        if (ret != TC_IOT_SUCCESS) {
+            tc_iot_hal_printf("refresh token failed, trouble shooting guide: " "%s#%d\n", TC_IOT_TROUBLE_SHOOTING_URL, ret);
+            return 0;
+        }
+        tc_iot_hal_printf("request username and password for mqtt success.\n");
+    } else {
+        tc_iot_hal_printf("username & password using: %s %s\n", p_client_config->device_info.username, p_client_config->device_info.password);
+    }
+
+    run_shadow(&g_client_config);
+
+    return 0;
+}
+
+int run_shadow(tc_iot_shadow_config * p_client_config) {
+    int timeout;
+    int ret = 0;
+    char buffer[512];
+    int buffer_len = sizeof(buffer);
+    char reported[256];
+    char desired[256];
+    tc_iot_shadow_client client;
+    tc_iot_shadow_client* p_shadow_client = &client;
+
+    tc_iot_hal_printf("constructing mqtt shadow client.\n");
+    ret = tc_iot_shadow_construct(p_shadow_client, p_client_config);
+    if (ret != TC_IOT_SUCCESS) {
+        tc_iot_hal_printf("construct shadow client failed, trouble shooting guide: " "%s#%d\n", TC_IOT_TROUBLE_SHOOTING_URL, ret);
+        return 0;
+    }
+
+    tc_iot_hal_printf("construct mqtt shadow client success.\n");
+    timeout = TC_IOT_CONFIG_COMMAND_TIMEOUT_MS;
+    tc_iot_hal_printf("yield waiting for server push.\n");
+    tc_iot_shadow_yield(p_shadow_client, timeout);
+    tc_iot_hal_printf("yield waiting for server finished.\n");
+
+    tc_iot_hal_printf("[c->s] shadow_get\n");
+    tc_iot_shadow_get(p_shadow_client);
+    tc_iot_shadow_yield(p_shadow_client, timeout);
+
+    snprintf(reported, sizeof(reported), 
+            "{\"string\":\"%s\",\"number\":%d,\"double\":%f,\"bool\":%s,\"obj\":%s}",
+            tc_iot_json_inline_escape(buffer, buffer_len, "A string \"\r\n"),
+            12345,3.14159, TC_IOT_JSON_TRUE, TC_IOT_JSON_NULL);
+    snprintf(desired, sizeof(desired), 
+            "{\"string\":\"%s\",\"number\":%d,\"double\":%f,\"bool\":%s,\"obj\":%s}",
+            tc_iot_json_inline_escape(buffer, buffer_len, "Hello, world!"),
+            700,10000.1234, TC_IOT_JSON_FALSE, TC_IOT_JSON_NULL);
+
+    ret =  tc_iot_shadow_doc_pack_for_update(buffer, buffer_len, p_shadow_client, reported, desired);
+    tc_iot_hal_printf("[c->s] shadow_update_all\n%s\n", buffer);
+    tc_iot_shadow_update(p_shadow_client, buffer);
+    tc_iot_shadow_yield(p_shadow_client, timeout);
+
+    ret =  tc_iot_shadow_doc_pack_for_update(buffer, buffer_len, p_shadow_client, reported, NULL);
+    tc_iot_hal_printf("[c->s] shadow_update_reported\n%s\n", buffer);
+    tc_iot_shadow_update(p_shadow_client, buffer);
+    tc_iot_shadow_yield(p_shadow_client, timeout);
+
+    ret =  tc_iot_shadow_doc_pack_for_update(buffer, buffer_len, p_shadow_client, NULL, desired);
+    tc_iot_hal_printf("[c->s] shadow_update_desired\n%s\n", buffer);
+    tc_iot_shadow_update(p_shadow_client, buffer);
+    tc_iot_shadow_yield(p_shadow_client, timeout);
+
+    ret =  tc_iot_shadow_doc_pack_for_delete(buffer, buffer_len, p_shadow_client, TC_IOT_JSON_NULL, TC_IOT_JSON_NULL);
+    tc_iot_hal_printf("[c->s] shadow_delete\n%s\n",buffer);
+    tc_iot_shadow_delete(p_shadow_client, buffer);
+    tc_iot_shadow_yield(p_shadow_client, timeout);
+
+    tc_iot_hal_printf("Stopping\n");
+    tc_iot_shadow_destroy(p_shadow_client);
+    tc_iot_hal_printf("Exit success.\n");
+    return 0;
+}
+
---
+++ samples/linux/light/tc_iot_device_config.h
@@ -0,0 +1,96 @@
+#ifndef TC_IOT_DEVICE_CONFIG_H
+#define TC_IOT_DEVICE_CONFIG_H
+
+// 服务端获取动态username和password接口
+// 地址格式为：<机房标识>.auth-device-iot.tencentcloudapi.com/device
+// 广州=gz
+// 北京=bj
+// ...
+// 以下为广州机房的接口地址：
+#ifdef ENABLE_TLS
+#define TC_IOT_CONFIG_AUTH_API_URL "https://gz.auth-device-iot.tencentcloudapi.com/device"
+#else
+#define TC_IOT_CONFIG_AUTH_API_URL "http://gz.auth-device-iot.tencentcloudapi.com/device"
+#endif
+
+/************************************************************************/
+/**********************************必填项********************************/
+
+#ifdef ENABLE_TLS
+// 是否启用TLS用于MQTT请求
+#define TC_IOT_CONFIG_USE_TLS 1
+#else
+#define TC_IOT_CONFIG_USE_TLS 0
+#endif
+
+#if TC_IOT_CONFIG_USE_TLS
+// MQ服务的TLS端口一般为8883
+#define TC_IOT_CONFIG_SERVER_PORT 8883
+#else
+// MQ服务的默认端口一般为1883
+#define TC_IOT_CONFIG_SERVER_PORT 1883
+#endif
+
+
+// 以下配置需要先在官网创建产品和设备，然后获取相关信息更新
+// MQ服务地址，可以在产品“基本信息页”->“mqtt链接地址”位置找到。
+#define TC_IOT_CONFIG_SERVER_HOST "mqtt-1e8w58ou4.ap-guangzhou.mqtt.tencentcloudmq.com"
+//#define TC_IOT_CONFIG_SERVER_HOST "localhost"
+// 产品id，可以在产品“基本信息页”->“产品id”位置找到
+#define TC_IOT_CONFIG_DEVICE_PRODUCT_ID "iot-fdxt0fie"
+
+// 设备密钥，可以在产品“设备管理”->“设备证书”->“Device Secret”位置找到
+#define TC_IOT_CONFIG_DEVICE_SECRET "00000000000000000000000000000000"
+
+// 设备名称，可以在产品“设备管理”->“设备名称”位置找到
+#define TC_IOT_CONFIG_DEVICE_NAME "device006"
+// client id，
+// 由两部分组成，组成形式为“Instanceid@DeviceID”，ClientID 的长度不超过 64个字符
+// ，请不要使用不可见字符。其中
+// Instanceid 为 IoT MQ 的实例 ID。
+// DeviceID 为每个设备独一无二的标识，由业务方自己指定，需保证全局唯一，例如每个
+// 传感器设备的序列号。
+#define TC_IOT_CONFIG_DEVICE_CLIENT_ID "mqtt-1e8w58ou4@" TC_IOT_CONFIG_DEVICE_NAME
+
+/************************************************************************/
+/**********************************选填项********************************/
+// 关于username和password：
+// 1)如果是通过TC_IOT_CONFIG_AUTH_API_URL接口，动态获取的，以下两个参数可不用填写
+// 2)如果有预先申请好的固定username和password，可以把获取到的固定参数填写到如下位置
+#define TC_IOT_CONFIG_DEVICE_USER_NAME ""
+#define TC_IOT_CONFIG_DEVICE_PASSWORD ""
+/************************************************************************/
+
+
+// connect、publish、subscribe、unsubscribe 
+// 等命令执行超时时长，单位是毫秒
+#define TC_IOT_CONFIG_COMMAND_TIMEOUT_MS  2000
+// keep alive 间隔时长，单位是秒
+#define TC_IOT_CONFIG_KEEP_ALIVE_INTERVAL_SEC  60
+// 网络故障或服务端超时，是否自动重连
+#define TC_IOT_CONFIG_AUTO_RECONNECT 0
+
+#define TC_IOT_CONFIG_CLEAN_SESSION 1
+
+
+// shadow下行消息topic，mq服务端的响应和下行推送，
+// 都会发布到 "shadow/get/<product id>/<device name>" 这个topic
+// 客户端只需要订阅这个topic即可
+#define TC_IOT_SUB_TOPIC_PREFIX "shadow/get/"
+#define TC_IOT_SUB_TOPIC_FMT TC_IOT_SUB_TOPIC_PREFIX "%s/%s"
+#define TC_IOT_SUB_TOPIC_DEF TC_IOT_SUB_TOPIC_PREFIX TC_IOT_CONFIG_DEVICE_PRODUCT_ID "/" TC_IOT_CONFIG_DEVICE_NAME
+// shadow上行消息topic，客户端请求服务端的消息，发到到这个topic即可
+// topic格式"shadow/update/<product id>/<device name>"
+#define TC_IOT_PUB_TOPIC_PREFIX "shadow/update/"
+#define TC_IOT_PUB_TOPIC_FMT TC_IOT_PUB_TOPIC_PREFIX "%s/%s"
+#define TC_IOT_PUB_TOPIC_DEF TC_IOT_PUB_TOPIC_PREFIX TC_IOT_CONFIG_DEVICE_PRODUCT_ID "/" TC_IOT_CONFIG_DEVICE_NAME
+
+// tls 相关配置
+// 根证书路径
+#define TC_IOT_CONFIG_ROOT_CA NULL
+// 客户端证书路径
+#define TC_IOT_CONFIG_CLIENT_CRT NULL
+// 客户端私钥路径
+#define TC_IOT_CONFIG_CLIENT_KEY NULL
+
+#endif /* end of include guard */
