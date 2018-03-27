ab53848990d94388028308ca09e1a911c733488a
huxinbang <huxinbang@gmail.com>
2018-03-27 16:43:35 +0800

    commit doc

--- docs/sdk/search/all_b.js
+++ docs/sdk/search/all_b.js
@@ -4,10 +4,10 @@ var searchData=
   ['last_5fsent',['last_sent',['../struct__tc__iot__mqtt__client.html#aa150390c03e6b46de893616a1d13cdfd',1,'_tc_iot_mqtt_client']]],
   ['len',['len',['../struct__tc__iot__sha256__t.html#ae9fe54d205bba043315fe6bababe19b9',1,'_tc_iot_sha256_t::len()'],['../struct__tc__iot__yabuffer__t.html#ac5755b8a6a18a659931ac2f82fc6d707',1,'_tc_iot_yabuffer_t::len()']]],
   ['length',['length',['../struct__tc__iot__property.html#a7c342531a817bdaa9cefb2b69c81796f',1,'_tc_iot_property']]],
-  ['log_5fdebug',['LOG_DEBUG',['../tc__iot__log_8h.html#a754b3d074e0af4ad3c7b918dd77ecb2d',1,'tc_iot_log.h']]],
-  ['log_5ferror',['LOG_ERROR',['../tc__iot__log_8h.html#ad4a9117ce894e3319e903142347a0f63',1,'tc_iot_log.h']]],
-  ['log_5ffatal',['LOG_FATAL',['../tc__iot__log_8h.html#a89cecd05aeaafc715ebc091cf74d10fe',1,'tc_iot_log.h']]],
-  ['log_5finfo',['LOG_INFO',['../tc__iot__log_8h.html#a378e28bfcb78d17285210d6bbb70a083',1,'tc_iot_log.h']]],
-  ['log_5ftrace',['LOG_TRACE',['../tc__iot__log_8h.html#a58c994180e09d2f6400133b19b1d7036',1,'tc_iot_log.h']]],
-  ['log_5fwarn',['LOG_WARN',['../tc__iot__log_8h.html#add82efa459e0af380b68522c29b9fd44',1,'tc_iot_log.h']]]
+  ['log_5fdebug',['TC_IOT_LOG_DEBUG',['../tc__iot__log_8h.html#a754b3d074e0af4ad3c7b918dd77ecb2d',1,'tc_iot_log.h']]],
+  ['log_5ferror',['TC_IOT_LOG_ERROR',['../tc__iot__log_8h.html#ad4a9117ce894e3319e903142347a0f63',1,'tc_iot_log.h']]],
+  ['log_5ffatal',['TC_IOT_LOG_FATAL',['../tc__iot__log_8h.html#a89cecd05aeaafc715ebc091cf74d10fe',1,'tc_iot_log.h']]],
+  ['log_5finfo',['TC_IOT_LOG_INFO',['../tc__iot__log_8h.html#a378e28bfcb78d17285210d6bbb70a083',1,'tc_iot_log.h']]],
+  ['log_5ftrace',['TC_IOT_LOG_TRACE',['../tc__iot__log_8h.html#a58c994180e09d2f6400133b19b1d7036',1,'tc_iot_log.h']]],
+  ['log_5fwarn',['TC_IOT_LOG_WARN',['../tc__iot__log_8h.html#add82efa459e0af380b68522c29b9fd44',1,'tc_iot_log.h']]]
 ];
--- docs/sdk/search/defines_3.js
+++ docs/sdk/search/defines_3.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['log_5fdebug',['LOG_DEBUG',['../tc__iot__log_8h.html#a754b3d074e0af4ad3c7b918dd77ecb2d',1,'tc_iot_log.h']]],
-  ['log_5ferror',['LOG_ERROR',['../tc__iot__log_8h.html#ad4a9117ce894e3319e903142347a0f63',1,'tc_iot_log.h']]],
-  ['log_5ffatal',['LOG_FATAL',['../tc__iot__log_8h.html#a89cecd05aeaafc715ebc091cf74d10fe',1,'tc_iot_log.h']]],
-  ['log_5finfo',['LOG_INFO',['../tc__iot__log_8h.html#a378e28bfcb78d17285210d6bbb70a083',1,'tc_iot_log.h']]],
-  ['log_5ftrace',['LOG_TRACE',['../tc__iot__log_8h.html#a58c994180e09d2f6400133b19b1d7036',1,'tc_iot_log.h']]],
-  ['log_5fwarn',['LOG_WARN',['../tc__iot__log_8h.html#add82efa459e0af380b68522c29b9fd44',1,'tc_iot_log.h']]]
+  ['log_5fdebug',['TC_IOT_LOG_DEBUG',['../tc__iot__log_8h.html#a754b3d074e0af4ad3c7b918dd77ecb2d',1,'tc_iot_log.h']]],
+  ['log_5ferror',['TC_IOT_LOG_ERROR',['../tc__iot__log_8h.html#ad4a9117ce894e3319e903142347a0f63',1,'tc_iot_log.h']]],
+  ['log_5ffatal',['TC_IOT_LOG_FATAL',['../tc__iot__log_8h.html#a89cecd05aeaafc715ebc091cf74d10fe',1,'tc_iot_log.h']]],
+  ['log_5finfo',['TC_IOT_LOG_INFO',['../tc__iot__log_8h.html#a378e28bfcb78d17285210d6bbb70a083',1,'tc_iot_log.h']]],
+  ['log_5ftrace',['TC_IOT_LOG_TRACE',['../tc__iot__log_8h.html#a58c994180e09d2f6400133b19b1d7036',1,'tc_iot_log.h']]],
+  ['log_5fwarn',['TC_IOT_LOG_WARN',['../tc__iot__log_8h.html#add82efa459e0af380b68522c29b9fd44',1,'tc_iot_log.h']]]
 ];
