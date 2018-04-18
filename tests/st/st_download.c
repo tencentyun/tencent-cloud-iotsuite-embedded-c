38d837eb26bd254a60cb25771c2438f1d728449f
huxinbang <huxinbang@gmail.com>
2018-04-18 20:48:23 +0800

    add md5 to common lib.

--- tests/st/st_download.c
+++ tests/st/st_download.c
@@ -3,11 +3,18 @@
 #define TC_IOT_TROUBLE_SHOOTING_URL "https://git.io/vN9le"
 #define HTTPS_PREFIX "https"
 #define HTTPS_PREFIX_LEN (sizeof(HTTPS_PREFIX) - 1)
+typedef struct _tc_iot_down_helper{
+    int fd;
+    const char * digest;
+    tc_iot_md5_t md5_context;
+}tc_iot_download_helper;
 
 int my_http_download_callback(const void * context, const char * data, int data_len, int offset, int total) {
+    tc_iot_download_helper * helper = (tc_iot_download_helper *)context;
     /* tc_iot_hal_printf("\n[%d/%d]\n->%s", offset+data_len, total, data); */
     tc_iot_hal_printf("%d/%d\n", offset+data_len, total);
-    write(*(int *)context,data,data_len);
+    write(helper->fd,data,data_len);
+    tc_iot_md5_update(&helper->md5_context, data, data_len);
     /* tc_iot_hal_printf("%s", data); */
 }
 
@@ -17,6 +24,11 @@ int main(int argc, char** argv) {
     int fd = -1;
     const char * download_url = NULL;
     const char * filename = NULL;
+    unsigned char file_md5_digest[TC_IOT_MD5_DIGEST_SIZE];
+    char md5str[TC_IOT_MD5_DIGEST_SIZE*2 +1];
+    tc_iot_download_helper helper;
+    memset(file_md5_digest, 0, sizeof(file_md5_digest));
+    memset(&helper, 0, sizeof(helper));
 
     if (argc <= 0) {
 print_help:
@@ -64,12 +76,18 @@ print_help:
     if(fd != -1){
         tc_iot_hal_printf("%s file is created.\n", filename);
     }
-    ret = tc_iot_do_download(download_url, my_http_download_callback, &fd);
+
+    helper.fd = fd;
+    tc_iot_md5_init(&helper.md5_context);
+    ret = tc_iot_do_download(download_url, my_http_download_callback, &helper);
     if (ret != TC_IOT_SUCCESS) {
         tc_iot_hal_printf("ERROR: %s download as %s failed.\n", download_url, filename);
     } else {
-        tc_iot_hal_printf("%s download as %s success.\n", download_url, filename);
+        tc_iot_md5_finish(&helper.md5_context, file_md5_digest);
+        tc_iot_hal_printf("%s download as %s success\n", download_url, filename);
+        tc_iot_hal_printf("md5=%s\n", tc_iot_util_byte_to_hex(file_md5_digest, sizeof(file_md5_digest), md5str, sizeof(md5str)));
     }
+
     close(fd);
 }
 
---
+++ tests/st/st_md5.c
@@ -0,0 +1,31 @@
+#include "tc_iot_inc.h"
+
+void test(const char * data, const char * expected) {
+    int i = 0;
+    int len = 0;
+    unsigned char md5sum[TC_IOT_MD5_DIGEST_SIZE];
+    char md5sum_hexstr[2*TC_IOT_MD5_DIGEST_SIZE + 1];
+    if (data) {
+        len = strlen(data);
+    }
+    tc_iot_md5_digest(data, len, md5sum);
+    if (0 == strcmp(expected, tc_iot_util_byte_to_hex(md5sum, sizeof(md5sum), md5sum_hexstr, sizeof(md5sum_hexstr)))) {
+        tc_iot_hal_printf("%s == %s\n", expected,  md5sum_hexstr);
+    } else {
+        tc_iot_hal_printf("%s != %s\n", expected,  md5sum_hexstr);
+    }
+}
+
+int main(int argc, char const* argv[])
+{
+    test("The quick brown fox jumps over the lazy dog",
+            "9e107d9d372bb6826bd81d3542a419d6");
+    test("The quick brown fox jumps over the lazy dog.",
+            "e4d909c290d0fb1ca068ffaddf22cbd0");
+    test("",
+            "d41d8cd98f00b204e9800998ecf8427e");
+    test(NULL,
+            "d41d8cd98f00b204e9800998ecf8427e");
+    return 0;
+}
+
