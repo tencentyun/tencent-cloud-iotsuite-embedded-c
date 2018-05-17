d8a3dc556d94f7e335b8dc330273e3a058f59eba
huxinbang <huxinbang@gmail.com>
2018-05-17 14:41:58 +0800

    generic platform compile error check.

--- include/platform/generic/tc_iot_platform.h
+++ include/platform/generic/tc_iot_platform.h
@@ -18,6 +18,11 @@
 #define tc_iot_hal_printf printf
 #define tc_iot_hal_snprintf snprintf
 
+#define htons(a) a
+#define htonl(a) a
+#define ntohs(a) a
+#define ntohl(a) a
+
 struct _tc_iot_timer { long end_time; };
 
 #endif /* end of include guard */
