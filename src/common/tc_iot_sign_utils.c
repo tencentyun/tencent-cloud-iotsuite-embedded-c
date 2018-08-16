7a01d0eda03777149a0ca92c30f111c099513355
huxinbang <huxinbang@gmail.com>
2018-08-16 14:56:30 +0800

    add sign trace.

--- src/common/tc_iot_sign_utils.c
+++ src/common/tc_iot_sign_utils.c
@@ -1,5 +1,22 @@
 #include "tc_iot_inc.h"
 
+
+#if defined(TC_IOT_LOG_TRACE)
+#define TC_IOT_SIGN_UTILS_TRACE(a,b) _tc_iot_sign_trace_print(a,b)
+static void _tc_iot_sign_trace_print(const char * str, int len) {
+    int i = 0;
+    if (len == -1) {
+        tc_iot_hal_printf("%s", str);
+    } else {
+        for (i = 0; i < len; i++) {
+            tc_iot_hal_printf("%c", str[i]);
+        }
+    }
+}
+#else
+#define TC_IOT_SIGN_UTILS_TRACE(a,b) 
+#endif
+
 int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret, const char * format, ...) {
     va_list ap;
     const unsigned char * pos  = (const unsigned char *)format;
@@ -10,7 +27,9 @@ int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret
 
     tc_iot_hmac_sha256_t hmac;
     tc_iot_hmac_sha256_init(&hmac, (unsigned char *)secret, strlen(secret));
-    TC_IOT_LOG_TRACE("secret=%s,format=%s",secret, format);
+
+    TC_IOT_SIGN_UTILS_TRACE((const char *)secret, -1);
+    TC_IOT_SIGN_UTILS_TRACE((const char *)"|", -1);
 
     if (output_len < TC_IOT_SHA256_DIGEST_SIZE) {
         return TC_IOT_BUFFER_OVERFLOW;
@@ -23,6 +42,7 @@ int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret
             pos++;
             if ((pos-1) > prev) {
                 tc_iot_sha256_update(&(hmac.sha), prev, pos-1-prev);
+                TC_IOT_SIGN_UTILS_TRACE((const char *)prev, pos-1-prev);
                 prev = pos;
             }
             switch(*pos) {
@@ -32,6 +52,7 @@ int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret
                     tc_iot_hal_snprintf(format_str, sizeof(format_str), "%%%c", *pos);
                     tc_iot_hal_snprintf(num_buf, sizeof(num_buf), format_str, va_arg(ap, int));
                     tc_iot_sha256_update(&(hmac.sha), (const unsigned char *)num_buf, strlen(num_buf));
+                    TC_IOT_SIGN_UTILS_TRACE(num_buf, -1);
                     break;
                 case 'u':
                 case 'o':
@@ -40,6 +61,7 @@ int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret
                     tc_iot_hal_snprintf(format_str, sizeof(format_str), "%%%c", *pos);
                     tc_iot_hal_snprintf(num_buf, sizeof(num_buf), format_str, va_arg(ap, unsigned int));
                     tc_iot_sha256_update(&(hmac.sha), (const unsigned char *)num_buf, strlen(num_buf));
+                    TC_IOT_SIGN_UTILS_TRACE(num_buf, -1);
                     break;
                 case 'f':
                 case 'F':
@@ -51,10 +73,12 @@ int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret
                     tc_iot_hal_snprintf(format_str, sizeof(format_str), "%%%c", *pos);
                     tc_iot_hal_snprintf(num_buf, sizeof(num_buf), format_str, va_arg(ap, double));
                     tc_iot_sha256_update(&(hmac.sha), (const unsigned char *)num_buf, strlen(num_buf));
+                    TC_IOT_SIGN_UTILS_TRACE(num_buf, -1);
                     break;
                 case 's':
                     var_str = va_arg(ap, const char *);
                     tc_iot_sha256_update(&(hmac.sha), (const unsigned char *)var_str, strlen(var_str));
+                    TC_IOT_SIGN_UTILS_TRACE(var_str, -1);
                     break;
 
                 case 'l':
@@ -65,6 +89,7 @@ int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret
                             tc_iot_hal_snprintf(format_str, sizeof(format_str), "%%l%c", *pos);
                             tc_iot_hal_snprintf(num_buf, sizeof(num_buf), format_str, va_arg(ap, long int));
                             tc_iot_sha256_update(&(hmac.sha), (const unsigned char *)num_buf, strlen(num_buf));
+                            TC_IOT_SIGN_UTILS_TRACE(num_buf, -1);
                             break;
                         case 'u':
                         case 'o':
@@ -73,6 +98,7 @@ int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret
                             tc_iot_hal_snprintf(format_str, sizeof(format_str), "%%l%c", *pos);
                             tc_iot_hal_snprintf(num_buf, sizeof(num_buf), format_str, va_arg(ap, unsigned long int));
                             tc_iot_sha256_update(&(hmac.sha), (const unsigned char *)num_buf, strlen(num_buf));
+                            TC_IOT_SIGN_UTILS_TRACE(num_buf, -1);
                             break;
                         default:
                             TC_IOT_LOG_ERROR("unkown *pos=%c", *pos);
@@ -97,8 +123,10 @@ int tc_iot_calc_sign(unsigned char * output, int output_len, const char * secret
 
     if ((pos-1) > prev) {
         tc_iot_sha256_update(&(hmac.sha), prev, pos-1-prev);
+        TC_IOT_SIGN_UTILS_TRACE((const char *)prev, pos-1-prev);
     }
 
+    TC_IOT_SIGN_UTILS_TRACE((const char *)"\n", -1);
     tc_iot_hmac_sha256_finish(&hmac, NULL, 0);
 
     memcpy(output, hmac.digest, TC_IOT_SHA256_DIGEST_SIZE);
@@ -178,9 +206,7 @@ int tc_iot_calc_auth_sign(char* sign_out, int max_sign_len, const char* secret, 
        tc_iot_mem_usage_log("b64_buf", sizeof(b64_buf), ret);
     }
 
-    TC_IOT_LOG_TRACE("tc_iot_calc_auth_sign source clientId=%s&deviceName=%s&expire=%d&nonce=%d&productId=%s&timestamp=%d sec %s sig %s\n", 
-            client_id, device_name, (int)expire, (int)nonce,product_id, (int)timestamp,
-            secret, b64_buf);
+    TC_IOT_LOG_TRACE("signature %s", b64_buf);
 
     url_ret = tc_iot_url_encode(b64_buf, ret, sign_out, max_sign_len);
     if (url_ret < max_sign_len) {
@@ -222,9 +248,7 @@ int tc_iot_calc_mqtt_dynamic_sign(char* sign_out, int max_sign_len,
         return ret;
     }
 
-    TC_IOT_LOG_TRACE("tc_iot_calc_auth_sign source clientId=%s&deviceName=%s&nonce=%d&productId=%s&timestamp=%d sec %s sig %s\n", 
-            client_id, device_name, (int)nonce,product_id, (int)timestamp,
-            secret, sign_out);
+    /* TC_IOT_LOG_TRACE("signature %s\n", sign_out); */
 
     /* char b64_buf[TC_IOT_BASE64_ENCODE_OUT_LEN(TC_IOT_SHA256_DIGEST_SIZE)]; */
     /* int url_ret = 0; */
