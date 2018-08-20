85bd8c337bae4d840da2096a0bd146a30346768a
huxinbang <huxinbang@gmail.com>
2018-08-20 20:04:23 +0800

    add cross compile.

--- tools/cross-compile/arm-gcc-toolchain.cmake
+++ tools/cross-compile/arm-gcc-toolchain.cmake
@@ -7,14 +7,20 @@ elseif(UNIX OR APPLE)
     set(UTIL_SEARCH_CMD which)
 endif()
 
-# for other embedded systems.
-# set(TOOLCHAIN_PREFIX arm-none-eabi-)
 
 # ---- for arm-linux begin ----
+# cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cross-compile/arm-gcc-toolchain.cmake ../
+#
 # install: sudo apt install gcc-arm-linux-gnueabihf
-# cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/arm-gcc-toolchain.cmake ../
-set(TOOLCHAIN_PREFIX arm-linux-gnueabihf-)
+# set(TOOLCHAIN_PREFIX arm-linux-gnueabihf-)
+
+# install: sudo apt install gcc-arm-linux-gnueabi
+set(TOOLCHAIN_PREFIX arm-linux-gnueabi-)
+
+# for other embedded systems.
+# set(TOOLCHAIN_PREFIX arm-none-eabi-)
 # ---- for arm-linux end ----
+
 
 execute_process(
   COMMAND ${UTIL_SEARCH_CMD} ${TOOLCHAIN_PREFIX}gcc
@@ -22,19 +28,19 @@ execute_process(
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-set(ARM_TOOLCHAIN_DIR "")
-# or 
-# set(ARM_TOOLCHAIN_DIR "/path/to/your/custom/directory/bin")
-
 # Without that flag CMake is not able to pass test compilation check
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 
-set(CMAKE_OBJCOPY ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
-set(CMAKE_SIZE_UTIL ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
+# set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
+
+# set(ARM_TOOLCHAIN_DIR "")
+# or 
+# set(ARM_TOOLCHAIN_DIR "/path/to/your/custom/directory/bin")
+# set(CMAKE_OBJCOPY ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
+# set(CMAKE_SIZE_UTIL ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
 
 set(CMAKE_FIND_ROOT_PATH ${BINUTILS_PATH})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
